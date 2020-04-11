704c4e483170e1679bf0528199ecec814f7bb6d3
Andres Campuzano <campuxano88@gmail.com>
2020-04-11 06:35:05 -0500

    Updating readme.

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # API-jQuery-to-JavaScript-VideoPlatform
-I'm using API yts.am to print in DOM real movie's information using JS and jQuery. 
 
-# What I learned
-- How to bring information from multiples API.
-- Render API in DOM.
-- Optimize code and functions in JS.
-- Develop with JS without relying on jQuery.
-- LocalStorage to reduce data usage.
+I'm using API yts.am to print in DOM real movie's information using JS and jQuery. You can try the demo [here](https://andrescampuzano.github.io/API-jQuery-to-JavaScript-VideoPlatform/)
+
+## What I learned
+
+-   How to bring information from multiples API.
+-   Render API in DOM.
+-   Optimize code and functions in JS.
+-   Develop with JS without relying on jQuery.
+-   LocalStorage to reduce data usage.
+
+### Preview
+
+![Preview image](https://raw.githubusercontent.com/AndresCampuzano/API-jQuery-to-JavaScript-VideoPlatform/master/sc01.png)
--- src/js/home.js
+++ src/js/home.js
@@ -1,51 +1,34 @@
-console.log('안녕하세요 세계!');
-
-//This is not related with the project, but is interesting, It brings a random user.
-fetch("https://randomuser.me/api/")
-  .then(function (response) {
-    console.log(response);
-    return response.json();
-  })
-  .then(function (user) {
-    console.log("user", user.results[0].name.first, "is generated randomly by API.");
-  })
-  .catch(function () {
-    console.log("algo falló");
-  });
-
-  console.log('Movies will be rendered in some seconds, please wait.');
-
-  //↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ PROJECT ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+console.log('Movies will be rendered in some seconds.');
 
 (async function load() {
-  // action
-  // drama
-  // animation
+	// action
+	// drama
+	// animation
 
-  async function getData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    if (data.data.movie_count > 0) { // https://platzi.com/clases/1316-jquery-js/12338-manejo-de-errores/ 3.50
-      return data;
-    } 
-    // if there are no movies, here it goes;
-    throw new Error('No se encontro ningun resultado');
-  };
+	async function getData(url) {
+		const response = await fetch(url);
+		const data = await response.json();
+		if (data.data.movie_count > 0) {
+			// https://platzi.com/clases/1316-jquery-js/12338-manejo-de-errores/ 3.50
+			return data;
+		}
+		// if there are no movies, here it goes;
+		throw new Error('No se encontro ningun resultado');
+	}
 
-  const $form = document.getElementById('form');
-  const $home = document.getElementById('home');
-  const $featuringContainer = document.getElementById('featuring');
+	const $form = document.getElementById('form');
+	const $home = document.getElementById('home');
+	const $featuringContainer = document.getElementById('featuring');
 
-  function setAttributes($element, attributes) {
-    for ( const attribute in attributes) {
-      $element.setAttribute(attribute, attributes[attribute]);
-    };
-  };
+	function setAttributes($element, attributes) {
+		for (const attribute in attributes) {
+			$element.setAttribute(attribute, attributes[attribute]);
+		}
+	}
 
-  // Template movie found through form.
-  function featuringTemplate(peli) {
-    return (
-      `
+	// Template movie found through form.
+	function featuringTemplate(peli) {
+		return `
       <div class="featuring">
         <div class="featuring-image">
           <img src="${peli.medium_cover_image}" width="70" height="100" alt="">
@@ -55,160 +38,163 @@ fetch("https://randomuser.me/api/")
           <p class="featuring-album">${peli.title}</p>
         </div>
       </div>
-      `
-    );
-  };
+      `;
+	}
 
-  //Events
-  $form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    $home.classList.add('search-active');
-    const $loader = document.createElement('img');
-    setAttributes($loader, {
-      src: 'src/images/loader.gif',
-      height: '50px',
-      widht: '50px'
-    });
-    $featuringContainer.append($loader);
-    
-    const data = new FormData($form); //                               ↓↓↓↓   ↓   ↓↓↓↓                 ↓↓
-    try { // https://platzi.com/clases/1316-jquery-js/12338-manejo-de-errores/ 6.00
-      const {
-        data: {
-          movies: pelis
-        }
-      } = await getData(`https://yts.mx/api/v2/list_movies.json?limit=1&query_term=${data.get('name')}`);
-  
-      const HTMLString = featuringTemplate(pelis[0]);
-      $featuringContainer.innerHTML = HTMLString;
-    } catch(error) {
-      alert(error.message);
-      $loader.remove();
-      $home.classList.remove('search-active');
-    }
-  });
+	//Events
+	$form.addEventListener('submit', async (event) => {
+		event.preventDefault();
+		$home.classList.add('search-active');
+		const $loader = document.createElement('img');
+		setAttributes($loader, {
+			src: 'src/images/loader.gif',
+			height: '50px',
+			widht: '50px',
+		});
+		$featuringContainer.append($loader);
 
-  //Getting info from API
-  const {
-    data: {
-      movies: actionList
-    }
-  } = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
-  const {
-    data: {
-      movies: dramaList
-    }
-  } = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
-  const {
-    data: {
-      movies: animationList
-    }
-  } = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
-  
-  console.log(actionList, dramaList, animationList);
+		const data = new FormData($form); //                               ↓↓↓↓   ↓   ↓↓↓↓                 ↓↓
+		try {
+			// https://platzi.com/clases/1316-jquery-js/12338-manejo-de-errores/ 6.00
+			const {
+				data: { movies: pelis },
+			} = await getData(
+				`https://yts.mx/api/v2/list_movies.json?limit=1&query_term=${data.get(
+					'name'
+				)}`
+			);
 
+			const HTMLString = featuringTemplate(pelis[0]);
+			$featuringContainer.innerHTML = HTMLString;
+		} catch (error) {
+			alert(error.message);
+			$loader.remove();
+			$home.classList.remove('search-active');
+		}
+	});
 
+	//Getting info from API
+	const {
+		data: { movies: actionList },
+	} = await getData('https://yts.mx/api/v2/list_movies.json?genre=action');
+	const {
+		data: { movies: dramaList },
+	} = await getData('https://yts.mx/api/v2/list_movies.json?genre=drama');
+	const {
+		data: { movies: animationList },
+	} = await getData('https://yts.mx/api/v2/list_movies.json?genre=animation');
 
-  function videoItemTemplate(movie, category) {
-    return (
-      `<div class="primaryPlaylistItem" data-id="${movie.id}" data-category="${category}">
+	console.log(actionList, dramaList, animationList);
+
+	function videoItemTemplate(movie, category) {
+		return `<div class="primaryPlaylistItem" data-id="${movie.id}" data-category="${category}">
       <div class="primaryPlaylistItem-image">
         <img src="${movie.medium_cover_image}">
        </div>
        <h4 class="primaryPlaylistItem-title">
            ${movie.title}
        </h4>
-   </div>` 
-    );
-  };
+   </div>`;
+	}
 
-  function createtemplate(HTMLString) {
-    const html = document.implementation.createHTMLDocument();
-    html.body.innerHTML = HTMLString;
-    return html.body.children[0];
-  };
+	function createtemplate(HTMLString) {
+		const html = document.implementation.createHTMLDocument();
+		html.body.innerHTML = HTMLString;
+		return html.body.children[0];
+	}
 
-  function addEventClick($element) { //modal
-    $element.addEventListener('click', () => {
-      // alert('click');
-      showModal($element);
-    });
-  };
+	function addEventClick($element) {
+		//modal
+		$element.addEventListener('click', () => {
+			// alert('click');
+			showModal($element);
+		});
+	}
 
-  function renderMovieList(list, $container, category) {
-      $container.children[0].remove(); //deleting loading gift once the info is rendered.
-      list.forEach((movie) => {
-      // Printing information in DOM
-      const HTMLString = videoItemTemplate(movie, category);
-      const movieElement = createtemplate(HTMLString);
-      $container.append(movieElement);
-      addEventClick(movieElement); //Click event.
-    });    
-  };
+	function renderMovieList(list, $container, category) {
+		$container.children[0].remove(); //deleting loading gift once the info is rendered.
+		list.forEach((movie) => {
+			// Printing information in DOM
+			const HTMLString = videoItemTemplate(movie, category);
+			const movieElement = createtemplate(HTMLString);
+			$container.append(movieElement);
+			addEventClick(movieElement); //Click event.
+		});
+	}
 
-  
-  //Getting information from HTML DOM
-  const $actionContainer = document.getElementById('action');
-  renderMovieList(actionList ,$actionContainer, 'action')
-  
-  const $dramaContainer = document.getElementById('drama');
-  renderMovieList(dramaList ,$dramaContainer, 'drama')
+	//Getting information from HTML DOM
+	const $actionContainer = document.getElementById('action');
+	renderMovieList(actionList, $actionContainer, 'action');
 
-  const $animationContainer = document.getElementById('animation');
-  renderMovieList(animationList ,$animationContainer, 'animation')
-  
-  //Careful with #. when queryselector is used, # needs to be written.
-  
-  // Modal code.
-  const $modal = document.getElementById('modal');
-  const $overlay = document.getElementById('overlay');
-  const $hideModal = document.getElementById('hide-modal');
+	const $dramaContainer = document.getElementById('drama');
+	renderMovieList(dramaList, $dramaContainer, 'drama');
 
-  const $modalTitle = $modal.querySelector('h1');
-  const $modalImage = $modal.querySelector('img');
-  const $modalDescription = $modal.querySelector('p');
+	const $animationContainer = document.getElementById('animation');
+	renderMovieList(animationList, $animationContainer, 'animation');
 
+	//Careful with #. when queryselector is used, # needs to be written.
 
-  function findById(list, id) {
-    return list.find(movie => movie.id === parseInt(id, 10));
-  };
+	// Modal code.
+	const $modal = document.getElementById('modal');
+	const $overlay = document.getElementById('overlay');
+	const $hideModal = document.getElementById('hide-modal');
 
-  function findMovie(id, category) {
-    switch (category) {
-      case 'action' : {
-        return findById(actionList, id)
-      };
-      case 'drama' : {
-        return findById(dramaList, id)
-      };
-      default: {
-        return findById(animationList, id)
-      };
-    };
-  };
+	const $modalTitle = $modal.querySelector('h1');
+	const $modalImage = $modal.querySelector('img');
+	const $modalDescription = $modal.querySelector('p');
 
-  // showModal
-  function showModal($element) {
-    $overlay.classList.add('active');
-    $modal.style.animation = 'modalIn .8s forwards';
-    const id = $element.dataset.id;
-    const category = $element.dataset.category;
-    const data = findMovie(id, category);  
+	function findById(list, id) {
+		return list.find((movie) => movie.id === parseInt(id, 10));
+	}
 
-    $modalTitle.textContent = data.title;
-    $modalImage.setAttribute('src', data.medium_cover_image);
-    $modalDescription.textContent = data.description_full  
-  }
+	function findMovie(id, category) {
+		switch (category) {
+			case 'action': {
+				return findById(actionList, id);
+			}
+			case 'drama': {
+				return findById(dramaList, id);
+			}
+			default: {
+				return findById(animationList, id);
+			}
+		}
+	}
 
-  // hideModal
-  $hideModal.addEventListener('click', hideModal);
-  function hideModal() {
-    $overlay.classList.remove('active');
-    $modal.style.animation = 'modalOut .8s forwards'
-  };
+	// showModal
+	function showModal($element) {
+		$overlay.classList.add('active');
+		$modal.style.animation = 'modalIn .8s forwards';
+		const id = $element.dataset.id;
+		const category = $element.dataset.category;
+		const data = findMovie(id, category);
 
+		$modalTitle.textContent = data.title;
+		$modalImage.setAttribute('src', data.medium_cover_image);
+		$modalDescription.textContent = data.description_full;
+	}
 
+	// hideModal
+	$hideModal.addEventListener('click', hideModal);
+	function hideModal() {
+		$overlay.classList.remove('active');
+		$modal.style.animation = 'modalOut .8s forwards';
+	}
 })();
 
-
-
+//This is not related with the project, but is interesting, It brings a random user.
+// fetch('https://randomuser.me/api/')
+// 	.then(function (response) {
+// 		console.log(response);
+// 		return response.json();
+// 	})
+// 	.then(function (user) {
+// 		console.log(
+// 			'user',
+// 			user.results[0].name.first,
+// 			'is generated randomly by API.'
+// 		);
+// 	})
+// 	.catch(function () {
+// 		console.log('algo falló');
+// 	});
